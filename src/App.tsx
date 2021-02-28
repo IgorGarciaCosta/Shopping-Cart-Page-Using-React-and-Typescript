7fe68bf777a891c56c68cb78720e03a3740cd624
IgorGarciaCosta <igonildo7@gmail.com>
2021-02-28 10:36:55 -0300

    :airplane: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 
-function App() {
+//Components
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingIcons from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+//Styles
+import { Wrapper } from './App.styles'
+
+//types
+export type CartItemType = {
+  is: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  //i use 2 awaits because of the json convertion
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts);
+
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveToCart = () => null;
+
+  if(isLoading) return<LinearProgress />;//progress bar at the top
+  if(error) return <div>Something went wrong...</div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
 
 export default App;
+//npm start
